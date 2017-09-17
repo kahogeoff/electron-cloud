086774fb9bea717f52c0ff373859d8cfe917bf07
Geoffrey Chueng <kahogeoff@gmail.com>
2017-09-17 15:28:02 +0800

    Added the tray button

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # electron-cloud
-An electron-based, cross-platform app for using Apple's iCloud
+[![works badge](https://cdn.rawgit.com/nikku/works-on-my-machine/v0.2.0/badge.svg)](https://github.com/nikku/works-on-my-machine)
+
+An electron-based, cross-platform app for using Apple's iCloud. Improved the functionality of the original repo.
+
+## New feature(s)
+
+ - Added a tray button, so you will keep the others window active when you close the main window.
+
+## Copyright notice
+
+The copyright of all iCloud (tm) icons are belong to Apple Inc. I don't own any off them.
+
+## Credits
+
+ - [nikharris0](https://github.com/nikharris0/electron-cloud) (The original creator)
+ - [dtafalonso](https://dtafalonso.deviantart.com/) (Artist of the iCloud icon)
--- main.js
+++ main.js
@@ -1,44 +1,127 @@
 const electron = require('electron')
 // Module to control application life.
 const app = electron.app
+
 // Module to create native browser window.
 const BrowserWindow = electron.BrowserWindow
+
+const Dialog = electron.dialog
+const Menu = electron.Menu
+const Tray = electron.Tray
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
 
+app.showExitPrompt = true
+
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({ width: 800, height: 600, show: true, frame: false, webPreferences: { nodeIntegration: false, zoomFactor: 0.9 }});
+  mainWindow = new BrowserWindow({ width: 960, height: 720, show: true, autoHideMenuBar: true, frame: true, webPreferences: { nodeIntegration: false, zoomFactor: 0.9 }});
 
   // and load the index.html of the app.
-  mainWindow.loadURL(`https://www.icloud.com`)
+  mainWindow.loadURL(`https://www.icloud.com`);
 
-  // Open the DevTools.
-//  mainWindow.webContents.openDevTools()
+  mainWindow.on('close', function (e) {
+    if(app.showExitPrompt)
+    {
+      // Stop the close event
+      e.preventDefault();
+
+      // Show a dialog to ask for hide or quit
+      Dialog.showMessageBox({
+        type: "question",
+        buttons: ["Hide", "Quit", "Cancel"],
+        defaultId: 1,
+        title: "Hide or Quit?",
+        message: "Do you want to quit or just hide?",
+        cancelId: 2
+      }, function (response, checkboxChecked) {
+        switch (response) {
+          // Hide
+          case 0:
+            closeWindow();
+            break;
+
+          // Quit
+          case 1:
+            quitApplication();
+            break;
+
+          // Cancel
+          default:
+            break;
+        }
+      });
+    }
+    
+  })
 
   // Emitted when the window is closed.
   mainWindow.on('closed', function () {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
-    mainWindow = null
+    mainWindow = null;
   })
+}
+
+function setupTray() {
+  tray = new Tray("icon/iCloud_32.png");
+
+  const contextMenu = Menu.buildFromTemplate([
+    {click: toggleShow, label: 'Hide/Show', type: 'normal'},
+    { type: 'separator'},
+    {click: quitApplication, label: 'Quit', type: 'normal'}
+  ]);
+
+  tray.setContextMenu(contextMenu);
+  
+  tray.on('double-click', function (e, bounds) {
+    toggleShow();
+  })
+}
+
+function toggleShow() {
+  if(app.showExitPrompt)
+    {
+      closeWindow();
+    }else{
+      showWindow();
+    }
+}
+
+function showWindow() {
+  mainWindow.show();
+  app.showExitPrompt = true;
+}
+
+function closeWindow() {
+  app.showExitPrompt = false;
+  if(mainWindow != null) {
+    mainWindow.hide();
+  }
+}
+
+function quitApplication() {
+  closeWindow();
+  app.quit();
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+app.on('ready', function () {
+  createWindow();
+  setupTray();
+})
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
+
+  //if (process.platform !== 'darwin') {
+  //  app.quit()
+  //}
 })
 
 app.on('activate', function () {
